02aac11be3ef45da8fad61dd6d6cb01903136574
iitjee <iit.jee.help.me@gmail.com>
2017-02-11 19:36:36 +0530

    Create Arrays.js

---
+++ Syntax/Arrays.js
@@ -0,0 +1,21 @@
+
+Arrays are mutable and no need to give size beforehand
+
+var myArr = [];
+myArr[1] = "hi";
+myArr[2] = 3;
+
+
+
+METHODS:
+--------
+var reverseArray = myArr.reverse()
+var joinedString = fruits.join(); //default joiner is comma
+var joinedString = fruits.join("--"); //joiner is --    to use spl chars, escape with \
+var sortedArray  = myArr.sort()
+
+
+
+
+
+
