b46f5ceae44172a44b0da7709b0ab82e1be95d8f
iitjee <iit.jee.help.me@gmail.com>
2017-02-11 22:33:21 +0530

    Create 03 Challenge.js

---
+++ Syntax/Functions/03 Challenge.js
@@ -0,0 +1,48 @@
+create an array of social media icons
+
+
+(script.js)
+var socialMedia = {
+  facebook : 'http://facebook.com/viewsource',
+  twitter: 'http://twitter.com/planetoftheweb',
+  flickr: 'http://flickr.com/planetotheweb',
+  youtube: 'http://youtube.com/planetoftheweb'
+};
+
+var socialList = function() {
+  var  output = '<ul>', 
+    myList = document.querySelectorAll('.socialmediaicons');
+
+  for (var key in arguments[0]) {
+    output+= '<li><a href="' + socialMedia[key] + '">' +
+      '<img src="images/' + key + '.png" alt="icon for '+key+'">' +
+      '</a></li>';
+  }
+  output+= '</ul>';
+  
+  for (var i = myList.length - 1; i >= 0; i--) {
+    myList[i].innerHTML = output;
+  };
+}(socialMedia); //note that this is a self-calling function. i.e it's called the moment this is defined
+
+
+(styles.css)
+body {
+  font-family: 'Helvetica Neue', 'Helvetica', sans-serif;
+}
+
+.socialmediaicons ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.socialmediaicons ul li {
+  display: inline-block;
+  margin-left: 5px;
+}
+
+.socialmediaicons ul li img {
+  width: 30px;
+  border-radius: 50%;
+}
