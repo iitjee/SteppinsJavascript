a251a61d1897953fc2bedd604a040e4af9e6ee4a
iitjee <iit.jee.help.me@gmail.com>
2017-02-11 20:08:58 +0530

    Create Strings.js

---
+++ Syntax/Strings.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+.toUpperCase
+var myArr = myString.split(" ") //splits the string on basis of space as delimiter
+var position = myString.indexOf("groovy") //finding(searching) a word; if not found returns -1
+.lastIndexOf("groovy")
+
+var segment = myPhrase.slice(6, 11) //cutting(slicing) into substring starting from 6th index
+//similar methods
+.substring(start, end)
+.substr(start, end)
+
+/*   String comparison */
+//sorting order: ABC...XYZabcd...xyz (increasing order i.e least is A)
+ 
+
+
+
+
+
+
+
+
+
