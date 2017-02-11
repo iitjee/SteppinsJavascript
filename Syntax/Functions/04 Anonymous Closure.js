4822f48fb8d31b8e3755175d4e847279805d252c
iitjee <iit.jee.help.me@gmail.com>
2017-02-11 22:49:57 +0530

    Create 04 Anonymous Closure.js

---
+++ Syntax/Functions/04 Anonymous Closure.js
@@ -0,0 +1,29 @@
+var iKnow = function kung (){
+  console.log("hi");
+  }
+  
+kung() //fail bcos kung is not defined in global scope
+iKnow() //okay
+
+
+var iKnow = function kung (){
+  console.log("hi");
+  }()
+  //this has converted the function declaration into function expression
+  
+  //^ if the above function is called, the moment it's defined what's the point of it having a name? Yep!
+var iKnow = function  (){
+  console.log("hi");
+  }()
+//now this has become an anonymous function
+//however, you require 'kung' if you are doing something recursive
+
+//You can even remove the variable declaration
+(function() {
+  console.log('foo');
+})();
+//this is the anonymous closure
+//this way any variables created inside this function is not accessible by outside
+//this is a good way to protect => 'clos'ure
+
+
