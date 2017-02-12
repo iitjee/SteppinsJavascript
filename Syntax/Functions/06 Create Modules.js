b274fc4d155e5fc67ad8dedc0cb1b4458cf64f16
iitjee <iit.jee.help.me@gmail.com>
2017-02-12 07:59:37 +0530

    Create 06 Create Modules.js

---
+++ Syntax/Functions/06 Create Modules.js
@@ -0,0 +1,63 @@
+
+
+
+
+Namespacing lets you provide encapsulation
+
+  var ray = (function() {
+    var privateVar;
+    
+    return { //returns an object
+      speak: function() {
+        console.log('hello');
+      }
+    }
+  })
+  
+// one of the reasons it's nice to use a object, because I can pass the whole thing into my function, and
+// then parse each of the different values out.
+
+var ray = (function() {
+  return {
+    speak: function() { 
+      console.log(arguments[0].say);
+    }
+  }
+}
+
+//while calling, 
+ray.speak({say:'howdy'});
+
+//But if you forget to give the propery while calling foreg,
+ray.speak() //you called without argument => ERROR!!
+//We'll use a trick called short-circuit evaluation
+
+      var myArguments = arguments[0] || '';
+      console.log(myArguments.say); //this will make sure somwething is there in arguments
+      //though we get 'undefined' in console, better than ERROR isn't it? :)
+      
+      
+//you can improve it by
+      var myArguments = arguments[0] || '';
+      var statement = myArguments.say || 'hello'; //'hello' will now act as a default argument
+      console.log(statement);
+      
+//that might clutter. so we'll make a DEFAULTS object at a higher scop
+  var = (function() {
+    var DEFAULTS = {
+      say: 'hello';
+    }
+    return {
+    speak: function() { //nested function type
+      var myArguments = arguments[0] || '';
+      var statement = myArguments.say || DEFAULTS.say; 
+      console.log(statement);
+    }
+  }
+}
+// It's a good idea to set up defaults for your modules at the top of your objects. 
+//They're going to be easier to find there, and can make your methods useful even if someone forgets to set them.
+
+
+
+
