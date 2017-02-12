88bf9e375d1e95e2bd8de60adb30f60b07a5b961
iitjee <iit.jee.help.me@gmail.com>
2017-02-12 09:43:58 +0530

    Update 06 Create Modules.js

--- Syntax/Functions/06 Create Modules.js
+++ Syntax/Functions/06 Create Modules.js
@@ -60,4 +60,29 @@ ray.speak() //you called without argument => ERROR!!
 
 
 
+//Chaining Module method calls         
+          var ray = (function() {
+          var DEFAULTS = {
+            say: 'hello',
+            speed: 'normal'
+          }
+
+          return {
+            speak: function() {
+              var myArguments = arguments[0] || '';
+              var statement = myArguments.say || DEFAULTS.say;
+              console.log(statement);
+              return this;
+            },
+            run : function() {
+              var myArguments = arguments[0] || '';
+              var running = myArguments.speed || DEFAULTS.speed;
+              console.log('running...'+ running);
+              return this;
+            }
+          };
+        })();
+//calling
+ray.speak({ say: 'howdy' }).run().speak({ say: 'run faster' }).run({speed: 'faster'});
+
 
