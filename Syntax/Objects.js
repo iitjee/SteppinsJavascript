23dd7ca171c903a4e88f70c46160808e9a551be7
iitjee <iit.jee.help.me@gmail.com>
2017-03-09 19:05:07 +0530

    Update Objects.js

--- Syntax/Objects.js
+++ Syntax/Objects.js
@@ -9,3 +9,7 @@ player.score = 1000;
 //shorthand way
 var player = {name: "Fred", score: 1000}
 
+
+//JS is dynamically typed language:
+We didn't even defined the type of player object. This is the beauty of a dynamically typed language.
+You can completely change the shape of an object at any time.
