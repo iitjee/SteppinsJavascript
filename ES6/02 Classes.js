ad646b41133dc2a7354488dce45885da740b7985
iitjee <iit.jee.help.me@gmail.com>
2017-03-11 12:51:47 +0530

    Create 02 Classes.js

---
+++ ES6/02 Classes.js
@@ -0,0 +1,26 @@
+'use strict'
+
+There are four ways to create objects:
+1. Object Literals
+2. Constructor Functions
+3. Object.create (very raw)
+4. ES6 Classes
+
+
+//es6 way
+class Cat {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+  
+  speak() {
+    display('meow')
+  }
+}
+
+var myCat = new Cat('Fluffy', 'White');
+display(myCat);
+
+//This is a new way of creating classes, but under-the-hood they are created using the same 'object' and 'prototype' stuff
+//it's just syntactic sugar! (Yummy!!)
