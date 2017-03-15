b351ed0db978fca4ea74d6b322f953e083882ce1
iitjee <iit.jee.help.me@gmail.com>
2017-03-15 14:54:00 +0530

    Create Steppins.js

---
+++ Steppins.js
@@ -0,0 +1,10 @@
+
+
+
+
+/* Convert 'HTMLCollection' to 'Array' Type */
+  convArray = [].slice.call(yourHTMLCollectionObject));
+  convArray = Array.prototype.slice.call(yourHTMLCollectionObject)); //same
+  
+//used in document.getElementsByClassName(), to put arraymethods like 'map'
+
