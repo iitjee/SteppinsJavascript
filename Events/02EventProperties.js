6f36b7edb30c6f2b20cccf3669b2da70dd779304
iitjee <iit.jee.help.me@gmail.com>
2018-06-19 14:03:44 -0400

    Create 02EventProperties.js

---
+++ Events/02EventProperties.js
@@ -0,0 +1,6 @@
+Once you register and capture an event, you'll receive an event object back from the browser. The object you get might look different 
+depending on the version of the browser you're using. And that's important to remember, because you won't always be able to use all this 
+information. The data you receive will also depend on the type of event you requested. But there's a lot of info that's common to all 
+events. You can get a lot of info about your event, and the browser environment, so let's take a look at some of the information we get 
+back from the event object. I'm going to go back to the code for this page, and instead of just hitting an Alert right here, I'm going to 
+use console log.
