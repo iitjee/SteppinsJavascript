014da97b615d69a55dd416f0851f429046f4789c
iitjee <iit.jee.help.me@gmail.com>
2019-01-15 15:29:47 +0530

    Update Steppins.js

--- Steppins.js
+++ Steppins.js
@@ -8,3 +8,16 @@
   
 //used in document.getElementsByClassName(), to put arraymethods like 'map'
 
+
+
+
+/* copying to clipboard */
+function copyToClipboard(text){
+    var dummy = document.createElement("input");
+    document.body.appendChild(dummy);
+    dummy.setAttribute('value', text);
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
+copyToClipboard('Hello, World!')
