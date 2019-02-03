4ebea2f293ae6e99bb83b97e95db97a2941dd22f
iitjee <iit.jee.help.me@gmail.com>
2019-02-03 15:10:59 +0530

    Update Steppins.js

--- Steppins.js
+++ Steppins.js
@@ -9,6 +9,14 @@
 //used in document.getElementsByClassName(), to put arraymethods like 'map'
 
 
+//saving canvas element!
+//https://stackoverflow.com/questions/11112321/how-to-save-canvas-as-png-image
+var c=document.getElementById("myCanvas"); 
+var d=c.toDataURL("image/png");
+var w=window.open('about:blank','image from canvas');
+w.document.write("<img src='"+d+"' alt='from canvas'/>");
+
+
 
 
 /* copying to clipboard */
