063a61dbd7e17453dfb3eff23e50ee157450e981
iitjee <iit.jee.help.me@gmail.com>
2019-11-25 03:00:54 +0530

    Update 02 CallnApply.js

--- Syntax/Functions/02 CallnApply.js
+++ Syntax/Functions/02 CallnApply.js
@@ -3,9 +3,18 @@
 With Call and Apply, you can define the value of the this argument. Now if you remember from traditional function
 declarations, one of the problems I mentioned is that the this parameter is bound to the global object.
 
-unclear: 'Understanding-call-apply-invocation/148137/158397-4'
+- An indirect invocation
+- Define the value of this argument
+- Control: this and arguments
+- "Call" passes a value and "APply"  is just like "Call" except that it expect an array instead of a single value
 
+myFn.call(....)
+myFn.apply(....)
 
+a function as you know is not just name and some executing code.
+in js, when you create a function, it also has three more "sub functions" --- call(), apply(), bind()
+
+----> https://medium.com/@omergoldberg/javascript-call-apply-and-bind-e5c27301f7bb
 
 /* varargs: */
 //we will use the 'arguments' parameter
