b4fbf37d02efa1203be375c392b521cc196566e4
iitjee <iit.jee.help.me@gmail.com>
2019-11-25 15:14:44 +0530

    Update 08 Promises3.js

--- ES6/08 Promises3.js
+++ ES6/08 Promises3.js
@@ -54,3 +54,30 @@ new Promise(function(resolve, reject) {
 
 });
 
+The idea is that the result is passed through the chain of .then handlers.
+The whole thing works, because a call to promise.then returns a promise, so that we can call the next .then on it.
+When a handler returns a value, it becomes the result of that promise, so the next .then is called with it.
+A classic newbie error: technically we can also add many .then to a single promise. This is not chaining.
+
+let promise = new Promise(function(resolve, reject) {
+  setTimeout(() => resolve(1), 1000);
+});
+
+promise.then(function(result) {
+  alert(result); // 1
+  return result * 2;
+});
+
+promise.then(function(result) {
+  alert(result); // 1
+  return result * 2;
+});
+
+promise.then(function(result) {
+  alert(result); // 1
+  return result * 2;
+});
+
+
+What we did here is just several handlers to one promise. They don’t pass the result to each other, instead they process it independently.
+(read https://javascript.info/promise-chaining )
