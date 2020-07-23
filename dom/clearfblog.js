c48e3992b48b223d41af9a5f60436f9567193afa
iitjee <iit.jee.help.me@gmail.com>
2020-07-23 18:41:34 +0530

    Create clearfblog.js

---
+++ dom/clearfblog.js
@@ -0,0 +1,5 @@
+for(x of [...document.getElementsByClassName("_-xe")])
+{x.click();[...document.getElementsByClassName("_54nh")].slice(-1)[0].click();
+
+//give some time and then paste the below code
+for(y of [...document.getElementsByClassName("_42ft _4jy0 layerConfirm uiOverlayButton _4jy3 _4jy1 selected _51sy")]) y.click();
